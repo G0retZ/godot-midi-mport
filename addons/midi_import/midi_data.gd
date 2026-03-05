afae29a88b05c2e2d14d91ec78c54e66989bfa1e
Sergei Mitrofanov <goretz.m@gmail.com>
2026-03-05 21:41:12 +0000

    Refactor running status logic a bit
    For better readability.

--- addons/midi_import/midi_data.gd
+++ addons/midi_import/midi_data.gd
@@ -234,9 +234,9 @@ class Event:
 		var timestamp_data := MidiData._buffer_to_variable_int(bytes)
 		var status := bytes[timestamp_data.y]
 		var event_data := bytes.slice(timestamp_data.y + 1)
-		var use_last_status := false
+		var status_length := 1
 		if status < 0x80: # check for running status
-			use_last_status = true
+			status_length = 0
 			status = _last_status
 			event_data = bytes.slice(timestamp_data.y)
 		_last_status = status
@@ -272,9 +272,7 @@ class Event:
 		if on_event != null and on_event.velocity == 0:
 			event = MidiData.NoteOff.new(status, event_data)
 		event.delta_time = timestamp_data.x
-		event._full_size += timestamp_data.y + 1
-		if use_last_status:
-			event._full_size -= 1
+		event._full_size += timestamp_data.y + status_length
 		MidiData._log("        size: %d" % event._full_size)
 		return event
 
